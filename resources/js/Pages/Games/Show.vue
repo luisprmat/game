ff91f0279b22e83ca2ef53514787712a8f943c79
Luis Parrado <luisprmat@gmail.com>
2024-08-15 19:19:21 -0500

    Fill dashboard depending turn (alternate)

--- resources/js/Pages/Games/Show.vue
+++ resources/js/Pages/Games/Show.vue
@@ -1,8 +1,16 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
-const boardState = ref([-1, 0, 0, 0, 1, 0, 0, 0, 0])
+const boardState = ref([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+const xTurn = computed(
+    () => boardState.value.reduce((carry, value) => carry + value, 0) === 0
+)
+
+const fillSquare = (index) => {
+    boardState.value[index] = xTurn.value ? -1 : 1
+}
 </script>
 
 <template>
@@ -11,10 +19,11 @@ const boardState = ref([-1, 0, 0, 0, 1, 0, 0, 0, 0])
             class="mx-auto mt-12 grid w-0 min-w-fit grid-cols-3 gap-1.5 dark:text-gray-100"
         >
             <li
-                v-for="square in boardState"
+                v-for="(square, index) in boardState"
                 class="grid size-28 place-items-center bg-gray-300 sm:size-32 dark:bg-gray-700"
             >
                 <button
+                    @click="fillSquare(index)"
                     v-if="square === 0"
                     class="place-self-stretch bg-gray-200 transition-colors hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700"
                 ></button>
